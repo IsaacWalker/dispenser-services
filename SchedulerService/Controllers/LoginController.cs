db74c53e58d7c04ab35d3f9083b91ae7414677ad
IsaacWalker <isaacwalker1377@gmail.com>
2020-03-20 14:59:50 +0000

    Added Login Screen Controller

---
+++ SchedulerService/Controllers/LoginController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Web.Models;
+
+namespace Web.SchedulerService.Controllers
+{
+    public class LoginController : APIControllerBase
+    {
+        public LoginController(IServiceProvider serviceProvider, ILogger<LoginController> logger) : base(serviceProvider, logger)
+        {
+        }
+
+
+        [Route("login")]
+        public ViewResult Login()
+        {
+            return View("Views/Pages/Login.cshtml");
+        }
+
+
+        [Route("login/attempt")]
+        [HttpPost]
+        public IActionResult Attempt(LoginAttemptModel model)
+        {
+            return Ok();
+        }
+    }
+}
---
+++ Web.Models/LoginAttemptModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Web.Models
+{
+    public class LoginAttemptModel
+    {
+        public string InputEmailAddress { get; set; }
+
+
+        public string InputPassword { get; set; }
+
+
+        public bool RememberPasswordCheck { get; set; }
+    }
+}
