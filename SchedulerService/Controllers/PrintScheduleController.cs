56f8b185245c2cf5c4c2788826804ac24709b80f
IsaacWalker <isaacwalker1377@gmail.com>
2020-03-21 13:48:31 +0000

    Added controller for the schedule screen

---
+++ SchedulerService/Controllers/PrintScheduleController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Web.Models.ViewModels;
+
+namespace Web.SchedulerService.Controllers
+{
+    public class PrintScheduleController : APIControllerBase
+    {
+        public PrintScheduleController(IServiceProvider serviceProvider, ILogger<PrintScheduleController> logger) 
+            : base(serviceProvider, logger)
+        {
+        }
+
+
+        [Route("schedule")]
+        public ViewResult Index()
+        {
+            HomePageModel model = new HomePageModel()
+            {
+                BatchNumber = 5,
+                Status = "PRINTING",
+                ETA = DateTime.Now,
+                ODFs = new List<BatchODF>(),
+                NavbarModel = new NavbarPartialModel()
+                {
+                    NurseFirstName = "FIRST",
+                    Patients = new List<NavbarPatientModel>()
+                },
+                PrintJobId = Guid.NewGuid()
+            };
+
+            return View("~/Views/Pages/PrintSchedule.cshtml", model);
+        }
+    }
+}
